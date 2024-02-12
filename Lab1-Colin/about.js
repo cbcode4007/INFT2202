f531bb8d2a33a293ecb3af9a10b2e3715c25bf1b
Colin Bond <colin.bond@dcmail.ca>
2024-02-12 12:47:24 -0500

    Lab 1 Current Image and ICE 4 Files

--- Lab1-Colin/about.js
+++ Lab1-Colin/about.js
@@ -12,9 +12,8 @@ document.write('<div class="container my-5">');
 document.write('<h1>About Us</h1>');
 document.write('<div class="col-lg-8 px-0">');
 document.write('<hr class="col-1 my-4">');
-// TODO: Replace with current image of yourself
-document.write('<img src="cool_dog.jpg" alt="Nugget The Dog">');
+document.write('<img src="current_image.jpg" alt="Photo of Colin">');
 document.write('<hr class="col-1 my-4">');
-document.write('<p class="fs-5">The team behind Colin Labs consists only of its founder Colin Bond. Colin is a semester 4 (year 2 semester 2) Computer Programming and Analysis student at Durham College. His passions include programming (as you might expect), consuming others\' programming in the form of games or sites, and getting his daily exercise in with his dog (pictured below).</p>');
+document.write('<p class="fs-5">The team behind Colin Labs consists only of its founder Colin Bond. Colin is a semester 4 (year 2 semester 2) Computer Programming and Analysis student at Durham College. His passions include programming (as you might expect), consuming others\' programming in the form of games or sites, and getting his daily exercise in with his dog Nugget (pictured below).</p>');
 document.write('<hr class="col-1 my-4">');
 document.write('<img src="cool_dog.jpg" alt="Nugget The Dog">');
